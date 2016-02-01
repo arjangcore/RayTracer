7105ec074186fc581f52d0089be8b1186eecba0a
arjangcore <arjangcore@gmail.com>
2016-01-31 23:11:04 -0800

    First commit

---
+++ sphere.cpp
@@ -0,0 +1,69 @@
+//
+//  Framework for a raytracer
+//  File: sphere.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include "sphere.h"
+#include <iostream>
+#include <math.h>
+
+/************************** Sphere **********************************/
+
+Hit Sphere::intersect(const Ray &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    * 
+    * Insert calculation of ray/sphere intersection here. 
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+	Vector OC = (position - ray.O);
+	double oc2 = OC.length_2();
+	double ocDotD = OC.dot(ray.D);
+	double ocDotD2 = ocDotD*ocDotD;
+	double rad2 = oc2 - ocDotD2;
+	double r2 = r*r;
+	double orthodDist2 = rad2 - r2;
+	if (orthodDist2 > DBL_EPSILON)
+		return Hit::NO_HIT();
+
+  //  Vector OC = (position - ray.O).normalized();
+  //  if (OC.dot(ray.D) < 0.999) {
+  //      return Hit::NO_HIT();
+  //  }
+	double t = ocDotD - sqrt(r2 - orthodDist2);
+
+	Vector collisionPnt = ray.O + t * ray.D;
+    /****************************************************
+    * RT1.2: NORMAL CALCULATION
+    *
+    * Given: t, C, r
+    * Sought: N
+    * 
+    * Insert calculation of the sphere's normal at the intersection point.
+    ****************************************************/
+
+	Vector N = (collisionPnt - position);
+	N.normalize();
+    return Hit(t,N);
+}
